23435c7800b48923f956b24b71a52db08251c5d1
Emil Kristen <153062975+patters-09haulers@users.noreply.github.com>
2023-12-06 14:24:33 +0700

    Create database.php

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+class Database {
+    public static function save($data) {
+        // Simulate saving data to a database
+        // Actual implementation would involve a real database
+    }
+
+    public static function load() {
+        // Simulate loading data from a database
+        // Actual implementation would involve a real database
+    }
+}
+?>
