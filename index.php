96dc656384460c3e35779df7e4cd8e1d38ba38c2
Emil Kristen <153062975+patters-09haulers@users.noreply.github.com>
2023-12-06 14:22:40 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banking System</title>
+</head>
+<body>
+    <h1>Welcome to our Banking System</h1>
+    <?php include_once 'account.php'; ?>
+    <?php include_once 'transaction.php'; ?>
+    <?php include_once 'customer.php'; ?>
+    <?php include_once 'bank.php'; ?>
+    <?php include_once 'database.php'; ?>
+    <?php include_once 'index_controller.php'; ?>
+</body>
+</html>
